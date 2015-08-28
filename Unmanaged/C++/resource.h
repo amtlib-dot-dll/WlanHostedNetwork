b52fbc286863c69be2041110912e99d84fc8e527
amtlib-dot-dll <amtlib-dot-dll@users.noreply.github.com>
2015-08-28 22:17:37 +0800

    Create resource.h

---
+++ Unmanaged/C++/resource.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by WlanHostedNetworkC.rc
+//
+#define IDS_WINDOW_CAPTION              101
+#define IDS_HOTSPOT_RUNNING             102
+#define IDI_LOGO                        103
+#define IDS_HOTSPOT_STOPPED             103
+#define IDS_ERROR                       104
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
